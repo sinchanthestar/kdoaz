e9cefa3c2720438113f4af241d4538db7867e71e
a11bove <chiyomlbb25@gmail.com>
2026-01-03 23:41:22 +0800

    Update gamelist.lua

--- loader/gamelist.lua
+++ loader/gamelist.lua
@@ -1,8 +1,7 @@
 local gs = {
-  [7326934954] = "https://raw.githubusercontent.com/a11bove/kdoaz/refs/heads/main/loader/scripts/aiko99NITF.lua", -- 99 Nights in The Forest
-  [7671049560] = "https://raw.githubusercontent.com/a11bove/kdoaz/refs/heads/main/loader/scripts/aikoTheForge.lua", -- The Forge
-  [6466802953] = "https://raw.githubusercontent.com/a11bove/kdoaz/refs/heads/main/loader/scripts/aikoDns.lua", -- Diesel 'N Steel
-  [6701277882] = "https://raw.githubusercontent.com/a11bove/kdoaz/refs/heads/main/loader/scripts/aikoFishIt.lua", -- Fish It (GameId)
-  [121864768012064] = "https://raw.githubusercontent.com/a11bove/kdoaz/refs/heads/main/loader/scripts/aikoFishIt.lua", -- Fish It (PlaceId)
+  [7326934954] = "https://raw.githubusercontent.com/a11bove/kdoaz/refs/heads/main/loader/scripts/aw99NITF.lua", -- 99 Nights in The Forest
+  [7671049560] = "https://raw.githubusercontent.com/a11bove/kdoaz/refs/heads/main/loader/scripts/awTForge.lua", -- The Forge
+  [6466802953] = "https://raw.githubusercontent.com/a11bove/kdoaz/refs/heads/main/loader/scripts/awDns.lua", -- Diesel 'N Steel
+  [6701277882] = "https://raw.githubusercontent.com/a11bove/kdoaz/refs/heads/main/loader/scripts/awFishIt.lua", -- Fish It (GameId)
 }
 return gs
