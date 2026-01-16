8fd12aa7b52125bf53b2f91da82b020e9c6d40db
a11bove <chiyomlbb25@gmail.com>
2026-01-17 04:48:43 +0800

    Update gamelist.lua

--- loader/gamelist.lua
+++ loader/gamelist.lua
@@ -1,7 +1,8 @@
 local gs = {
-  [7326934954] = "https://raw.githubusercontent.com/a11bove/kdoaz/refs/heads/main/loader/scripts/aw99NITF.lua", -- 99 Nights in The Forest
-  [7671049560] = "https://raw.githubusercontent.com/a11bove/kdoaz/refs/heads/main/loader/scripts/awTForge.lua", -- The Forge
-  [6466802953] = "https://raw.githubusercontent.com/a11bove/kdoaz/refs/heads/main/loader/scripts/awDns.lua", -- Diesel 'N Steel
-  [6701277882] = "https://raw.githubusercontent.com/a11bove/kdoaz/refs/heads/main/loader/scripts/awFishIt.lua", -- Fish It (GameId)
+  [7326934954] = "https://raw.githubusercontent.com/a11bove/kdoaz/refs/heads/main/loader/scripts/aw99NITF.lua",
+  [7671049560] = "https://raw.githubusercontent.com/a11bove/kdoaz/refs/heads/main/loader/scripts/awTForge.lua",
+  [6466802953] = "https://raw.githubusercontent.com/a11bove/kdoaz/refs/heads/main/loader/scripts/awDns.lua",
+  [6701277882] = "https://raw.githubusercontent.com/a11bove/kdoaz/refs/heads/main/loader/scripts/awFishIt.lua",
+  [4777817887] = "https://raw.githubusercontent.com/a11bove/kdoaz/refs/heads/main/loader/scripts/awBbal.lua",
 }
 return gs
