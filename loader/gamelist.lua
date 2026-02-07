9ada6bb414f5fb2f2cd632ff1b908b44df26e4c4
sinchanthestar <anugrahalifiandi20@gmail.com>
2026-02-08 03:38:44 +0700

    Update URL for awFishIt.lua script

--- loader/gamelist.lua
+++ loader/gamelist.lua
@@ -2,7 +2,7 @@ local gs = {
   [7326934954] = "https://raw.githubusercontent.com/a11bove/kdoaz/refs/heads/main/loader/scripts/aw99NITF.lua",
   [7671049560] = "https://raw.githubusercontent.com/a11bove/kdoaz/refs/heads/main/loader/scripts/awTForge.lua",
   [6466802953] = "https://raw.githubusercontent.com/a11bove/kdoaz/refs/heads/main/loader/scripts/awDns.lua",
-  [6701277882] = "https://raw.githubusercontent.com/a11bove/kdoaz/refs/heads/main/loader/scripts/awFishIt.lua",
+  [6701277882] = "https://raw.githubusercontent.com/sinchanthestar/kdoaz/refs/heads/main/loader/scripts/awFishIt.lua",
   [4777817887] = "https://raw.githubusercontent.com/a11bove/kdoaz/refs/heads/main/loader/scripts/awBbal.lua",
 }
 return gs
